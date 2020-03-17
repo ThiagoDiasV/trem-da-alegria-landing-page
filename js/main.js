c8fe2e7fc21af51b8e149c042a256b6ead2541e4
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-17 17:31:45 -0300

    feat: pegagodic section

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//   $('a').on('click', function(event) {
+//     if (this.hash !== '') {
+//       event.preventDefault();
+
+//       var hash = this.hash;
+
+//       $('html', 'body').animate(
+//         {
+//           scrollTop: $(hash).offset().top
+//         },
+//         800,
+//         function() {
+//           console.log('entrei');
+//           window.location.hash = hash;
+//         }
+//       );
+//     }
+//   });
+// });
