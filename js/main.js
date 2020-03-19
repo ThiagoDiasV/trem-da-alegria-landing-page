aa4352697f60e7ade77ac84514e191867fd89b36
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-18 23:24:51 -0300

    feat: last sections and footer

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
-// $(document).ready(function() {
-//   $('a').on('click', function(event) {
-//     if (this.hash !== '') {
-//       event.preventDefault();
+// Get current year
+$('#year').text(new Date().getFullYear());
 
-//       var hash = this.hash;
+// Init scrollspy
+$('body').scrollspy({ target: '#main-nav' });
 
-//       $('html', 'body').animate(
-//         {
-//           scrollTop: $(hash).offset().top
-//         },
-//         800,
-//         function() {
-//           console.log('entrei');
-//           window.location.hash = hash;
-//         }
-//       );
-//     }
-//   });
-// });
+// Smooth scrolling
+$('#main-nav a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+    console.log($(hash).offset().top);
+    $('html').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+// Hide navbar when scroll down and show when scroll up
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('main-nav').style.top = '0';
+  } else {
+    document.getElementById('main-nav').style.top = '-100px';
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+// Hide responsive menu onclick
+$(function() {
+  var navMain = $('.navbar-collapse'); // avoid dependency on #id
+  // "a:not([data-toggle])" - to avoid issues caused
+  // when you have dropdown inside navbar
+  navMain.on('click', 'a:not([data-toggle])', null, function() {
+    navMain.collapse('hide');
+  });
+});
