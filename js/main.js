042cb72896df6015ff6c60f38e62b72e93bbbfb7
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-22 22:00:22 -0300

    feat: contact part

--- js/main.js
+++ js/main.js
@@ -43,16 +43,20 @@ $(function() {
 
 // Read more
 $('.more-trigger').on('click', function() {
-  if ($(this).text() === 'Leia mais') {
+  if (
+    $(this)
+      .text()
+      .trim() === 'Continuar lendo'
+  ) {
     $(this)
       .siblings('.more')
       .css('display', 'block');
-    $(this).text('Leia menos');
+    $(this).text('Diminuir texto');
   } else {
     $(this)
       .siblings('.more')
       .css('display', 'none');
-    $(this).text('Leia mais');
+    $(this).text('Continuar lendo');
     $('html').animate(
       {
         scrollTop: $(this)
@@ -65,3 +69,51 @@ $('.more-trigger').on('click', function() {
     );
   }
 });
+
+// Send email
+// get the form elements defined in your form HTML above
+
+var form = document.getElementById('my-form');
+var button = document.getElementById('my-form-button');
+var alertMessage = document.getElementById('submit-alert');
+
+// Success and Error functions for after the form is submitted
+
+function success() {
+  form.reset();
+  button.style = 'display: none ';
+  alertMessage.style = 'display: block';
+}
+
+function error() {
+  status.innerHTML =
+    'Ops! Tivemos um problema. Atualize a página e tente novamente.';
+}
+
+// handle the form submission event
+
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+  var data = new FormData(form);
+  ajax(form.method, form.action, data, success, error);
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
+// Enable alerts
+$('.alert').alert();
